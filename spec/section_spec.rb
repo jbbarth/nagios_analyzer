7451906c8b450c1ab4f82431d6c86208b1aab811
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-14 01:36:14 +0200

    Alias NagiosAnalyzer module to NA because 'include' doesn't seem to work in all rubies like it does in mri 1.9.2

--- spec/section_spec.rb
+++ spec/section_spec.rb
@@ -1,16 +1,17 @@
 require File.expand_path('../spec_helper',__FILE__)
 
-describe NagiosAnalyzer::Section do
-  include NagiosAnalyzer
+#alias NagiosAnalyzer to NA for convenience
+NA = NagiosAnalyzer
 
+describe NA::Section do
   before(:each) do
     file = File.expand_path('../data/status.dat',__FILE__)
-    status = Status.new(file)
+    status = NA::Status.new(file)
     @section = status.service_items.first
   end
 
   it "returns a hash with keys only" do
-    @section.should be_a(Section)
+    @section.should be_a(NA::Section)
     @section.keys.map(&:class).uniq.should == [Symbol]
   end
 
@@ -26,46 +27,46 @@ describe NagiosAnalyzer::Section do
 
   it "provides a :status key to know the status" do
     @section[:status].should == "WARNING"
-    Section.new("servicestatus {\ncurrent_state=0\n}")[:status].should == "OK"
-    Section.new("servicestatus {\ncurrent_state=2\n}")[:status].should == "CRITICAL"
-    Section.new("servicestatus {\ncurrent_state=3\n}")[:status].should == "UNKNOWN"
-    Section.new("hoststatus {\ncurrent_state=0\n}")[:status].should == "OK"
-    Section.new("hoststatus {\ncurrent_state=42\n}")[:status].should == "CRITICAL"
+    NA::Section.new("servicestatus {\ncurrent_state=0\n}")[:status].should == "OK"
+    NA::Section.new("servicestatus {\ncurrent_state=2\n}")[:status].should == "CRITICAL"
+    NA::Section.new("servicestatus {\ncurrent_state=3\n}")[:status].should == "UNKNOWN"
+    NA::Section.new("hoststatus {\ncurrent_state=0\n}")[:status].should == "OK"
+    NA::Section.new("hoststatus {\ncurrent_state=42\n}")[:status].should == "CRITICAL"
   end
 
   context "#sort" do
     it "places servicestatus'es after hoststatus'es" do
-      a = Section.new("servicestatus {\ncurrent_state=0\n}")
-      b = Section.new("hoststatus {\ncurrent_state=0\n}")
+      a = NA::Section.new("servicestatus {\ncurrent_state=0\n}")
+      b = NA::Section.new("hoststatus {\ncurrent_state=0\n}")
       [a,b].sort.should == [b,a]
     end
 
     it "places critical before unknown before warning before pending before dependent before ok" do
-      host = Section.new("hoststatus {\ncurrent_state=0\n}")
-      critical = Section.new("servicestatus {\ncurrent_state=2\n}")
-      unknown = Section.new("servicestatus {\ncurrent_state=3\n}")
-      warning = Section.new("servicestatus {\ncurrent_state=1\n}")
-      dependent = Section.new("servicestatus {\ncurrent_state=4\n}")
-      ok = Section.new("servicestatus {\ncurrent_state=0\n}")
+      host = NA::Section.new("hoststatus {\ncurrent_state=0\n}")
+      critical = NA::Section.new("servicestatus {\ncurrent_state=2\n}")
+      unknown = NA::Section.new("servicestatus {\ncurrent_state=3\n}")
+      warning = NA::Section.new("servicestatus {\ncurrent_state=1\n}")
+      dependent = NA::Section.new("servicestatus {\ncurrent_state=4\n}")
+      ok = NA::Section.new("servicestatus {\ncurrent_state=0\n}")
       [ok, unknown, dependent, critical, host, warning].sort.should == [host, critical, unknown, warning, dependent, ok]
     end
 
     it "sorts by host_name" do
-      a = Section.new("hoststatus {\ncurrent_state=0\nhost_name=a\n}")
-      b = Section.new("hoststatus {\ncurrent_state=0\nhost_name=b\n}")
+      a = NA::Section.new("hoststatus {\ncurrent_state=0\nhost_name=a\n}")
+      b = NA::Section.new("hoststatus {\ncurrent_state=0\nhost_name=b\n}")
       [b,a].sort.should == [a,b]
     end
 
     it "sorts by service_description" do
-      a = Section.new("hoststatus {\ncurrent_state=0\n}")
-      b = Section.new("servicestatus {\ncurrent_state=0\nservice_description=b\n}")
-      c = Section.new("servicestatus {\ncurrent_state=0\nservice_description=c\n}")
+      a = NA::Section.new("hoststatus {\ncurrent_state=0\n}")
+      b = NA::Section.new("servicestatus {\ncurrent_state=0\nservice_description=b\n}")
+      c = NA::Section.new("servicestatus {\ncurrent_state=0\nservice_description=c\n}")
       [c,b,a].sort.should == [a,b,c]
     end
 
     it "has no problem even with missing fields (hostname don't have service_description)" do
-      a = Section.new("hoststatus {\ncurrent_state=0\n}")
-      b = Section.new("hoststatus {\ncurrent_state=0\n}")
+      a = NA::Section.new("hoststatus {\ncurrent_state=0\n}")
+      b = NA::Section.new("hoststatus {\ncurrent_state=0\n}")
       [a,b].sort.should == [a,b]
     end
   end
