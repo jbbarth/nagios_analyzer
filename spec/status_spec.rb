528624ca2916938f4dfabd73a877f48243c16e16
Robin H. Johnson <robbat2@gentoo.org>
2023-01-20 21:15:47 -0800

    spec: fix outdated rspec
    - Replace outdated rspec syntax like `.have(N).$TYPE`
    - s/be_true/be_truthy/
    - s/be_false/be_falsey/
    - "should_not include" converted to explicit test
    
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -11,7 +11,7 @@ describe NagiosAnalyzer::Status do
     status.should be
     status.sections.should be_a(Array)
     status.sections.first.should include("created=")
-    status.should have(6).sections
+    status.sections.length.should equal(6)
   end
 
   it "provides a last_updated attribute" do
@@ -20,68 +20,68 @@ describe NagiosAnalyzer::Status do
 
   context "#scopes" do
     it "provides scopes to filter sections" do
-      status.should have(6).sections
-      status.should have(0).scopes
+      status.sections.length.should equal(6)
+      status.scopes.length.should equal(0)
     end
 
     it "tells if a section is in the scopes" do
       status.scopes << lambda{|s|s.include?("host_name=server-web")}
-      status.in_scope?(status.sections[2]).should be_true
-      status.in_scope?(status.sections[3]).should be_false
+      status.in_scope?(status.sections[2]).should be_truthy
+      status.in_scope?(status.sections[3]).should be_falsey
     end
 
     it "defines scope in the initialization" do
       status = NagiosAnalyzer::Status.new(@file, :include_ok => true,
                                           :scope => lambda{|s|s.include?("host_name=server-web")})
-      status.in_scope?(status.sections[2]).should be_true
-      status.in_scope?(status.sections[3]).should be_false
+      status.in_scope?(status.sections[2]).should be_truthy
+      status.in_scope?(status.sections[3]).should be_falsey
     end
   end
 
   context "#items, #service_items, #host_items, #hostcomment_items" do
     it "returns all items" do
-      status.should have(6).sections
-      status.should have(4).items         #don't return info{} and programstatus{} sections
+      status.sections.length.should equal(6)
+      status.items.length.should equal(4)         #don't return info{} and programstatus{} sections
       status.items.first.should be_a(NagiosAnalyzer::Section)
     end
 
     it "returns host items" do
-      status.should have(2).host_items    #4 = 2 host_items
+      status.host_items.length.should equal(2)    #4 = 2 host_items
       status.host_items.first[:type].should == "hoststatus"
     end
 
     it "returns service items" do
-      status.should have(2).service_items # ... + 2 service_items
+      status.service_items.length.should equal(2) # ... + 2 service_items
       status.service_items.first[:type].should == "servicestatus"
     end
 
     it "returns only service problems, keeping scopes on every items" do
-      status.should have(1).service_problems
-      status.should have(2).service_items
+      status.service_problems.length.should equal(1)
+      status.service_items.length.should equal(2)
     end
 
     it "returns host problems, currently none" do
-      status.should have(0).host_problems
+      status.host_problems.length.should equal(0)
     end
 
     it "resets cached attributes" do
-      status.should have(4).items
+      status.items.length.should equal(4)
       status.scopes << lambda{|s| s.start_with?("servicestatus")}
-      status.should have(4).items
+      status.items.length.should equal(4)
       status.reset_cache!
-      status.should have(2).items
-      status.should have(0).host_items
-      status.should have(2).service_items
+      status.items.length.should equal(2)
+      status.host_items.length.should equal(0)
+      status.service_items.length.should equal(2)
     end
   end
 
   context "without :include_ok option" do
     it "should filter items" do
       status = NagiosAnalyzer::Status.new(@file)
-      status.should have(1).items         #don't return info{} and programstatus{} sections
-      status.should have(0).host_items    #4 = 2 host_items
-      status.should have(1).service_items # ... + 2 service_items
-      status.service_items.first.should_not include("current_state=0")
+      status.items.length.should equal(1)         #don't return info{} and programstatus{} sections
+      status.host_items.length.should equal(0)    #4 = 2 host_items
+      status.service_items.length.should equal(1) # ... + 2 service_items
+	  status.service_items.first.current_state.should_not equal(0)
     end
   end
 
@@ -91,15 +91,15 @@ describe NagiosAnalyzer::Status do
     end
 
     it "sould have lists for all kinds of nagios sections" do
-      status.contactstatus_items.should have(1).items
-      status.hostcomment_items.should have(1).items
-      status.hostdowntime_items.should have(1).items
-      status.hoststatus_items.should have(1).items
-      status.info_items.should have(1).items
-      status.programstatus_items.should have(1).items
-      status.servicecomment_items.should have(1).items
-      status.servicedowntime_items.should have(1).items
-      status.servicestatus_items.should have(1).items
+      status.contactstatus_items.length.should equal(1)
+      status.hostcomment_items.length.should equal(1)
+      status.hostdowntime_items.length.should equal(1)
+      status.hoststatus_items.length.should equal(1)
+      status.info_items.length.should equal(1)
+      status.programstatus_items.length.should equal(1)
+      status.servicecomment_items.length.should equal(1)
+      status.servicedowntime_items.length.should equal(1)
+      status.servicestatus_items.length.should equal(1)
     end
 
     it "sould properly classify each kind of nagios sections" do
